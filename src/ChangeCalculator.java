47780890d16f1a959f127b5be1f352bc973b6882
IanLoam126 <league@WTS10.attlocal.net>
2018-03-26 18:26:23 -0700

    Test Scores (IP), Change Calculator, Hide the Creeper

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,23 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
+int n = Integer.parseInt(nickels);
+	 
+// Ask the user how many dimes they have, and convert their answer
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+        // Yee
+int d = Integer.parseInt(dimes);
+// Ask the user how many quarters they have, and convert their answer
+String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		// Yee
+int q = Integer.parseInt(quarters);
 
-		// Ask the user how many dimes they have, and convert their answer
-
-		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+// Calculate how much money the user has and save it in a double variable 
+  double cents = n*0.05 + d*0.10 + q*0.25;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "$"+cents);
 	}
 }
 
---
+++ src/TestScores.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+String score = JOptionPane.showInputDialog("What score did you get on your test? (Out of 100)");
+	double s = Double.parseDouble(score);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
