e9534926a33dae9596e39e2a10ecb161cf9a4ffb
League <League@AdminisorsiMac2.attlocal.net>
2018-04-02 18:26:07 -0700

    Test Score, Sleepy Head, String Methods1 (IP)

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,37 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
+		// JOptionPane.showConfirmDialog(null, "Is it a weekday?",
+		int i = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		if (i == 1) {
+			JOptionPane.showMessageDialog(null, "Sleep In");
+		} else {
+			int t = JOptionPane.showConfirmDialog(null, "Is it a vacation day?", "", JOptionPane.YES_NO_OPTION);
+			if (t == 1) {
+				JOptionPane.showMessageDialog(null, "WAKEY WAKEY EGGS AND BAKEY");
+			} else {
+				JOptionPane.showMessageDialog(null, "Sleep In");
+			}
+		}
 
+		/*
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		 */
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+
+	}
 }
--- src/TestScores.java
+++ src/TestScores.java
@@ -4,11 +4,21 @@ public class TestScores {
 public static void main(String[] args) {
 String score = JOptionPane.showInputDialog("What score did you get on your test? (Out of 100)");
 	double s = Double.parseDouble(score);
-	
-	
-	
-	
-	
+	if(s>80 && s<100) {
+	JOptionPane.showMessageDialog(null, "Good job studying!");	
+		}
+	if(s<60 && s>40) {
+	JOptionPane.showMessageDialog(null, "Study harder next time, you could do better.");	
+		}
+	if(s<40 && s>20) {
+		JOptionPane.showMessageDialog(null, "Spend more time studying and less time not studying. By doing this you could get a better score.");
+	}
+	if(s<20 && s>0) {
+		JOptionPane.showMessageDialog(null, "That score isn't acceptable. You must study much much harder next time, slacker!");
+	}
+	if(s>100) {
+		JOptionPane.showMessageDialog(null, "That is not out of 100. Maybe if you studied, you would know your numbers.");
+	}
 	
 	
 	
